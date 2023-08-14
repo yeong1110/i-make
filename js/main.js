3df9a8be2f18a673cefae56cffe102f6d2e0ddfc
yeong1110 <a0257942@gmail.com>
2023-08-15 01:13:27 +0900

    갤러리 스와이퍼 이슈 수정및 gsap 연습, 버튼 디자인 수정, include 수정

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(function(){
     setSwiper('.infinite-swiper', {
         // Optional parameters
-        slidesPerView: 3,
+        slidesPerView: 2,
+        spaceBetween: 10,
         // Navigation arrows
         navigation: {
         nextEl: '.swiper-button-next',
@@ -12,10 +13,15 @@ $(function(){
     
     setSwiper('.imagine-wrap',{
       direction: "vertical",
-      slidesPerView: 3,
-      spaceBetween: 10,
-
-        // spaceBetween: 10,
+      // autoHeight: true,
+      // allowTouchMove: false,
+      // slidesPerColumn: 3,
+      slidesPerView: "auto",
+      grid: {
+        rows: 2,
+      },
+      // slidesPerView: 3,
+      // spaceBetween: 10,
     })
 
 })
@@ -24,3 +30,43 @@ const setSwiper = (id, object) => {
     new Swiper(id, object);
   }
   
+  gsap.registerPlugin(ScrollTrigger);
+
+  // gsap.to(".intro-cont", {
+  //    scrollTrigger: {
+  //     trigger: ".intro-cont h2",
+  //     start: "top top",
+  //     end: "+=300",
+  //     scrub: 1,
+  //     pin: true,
+  //     markers: true
+  //   },
+  //   rotation: 400, 
+  //   duration: 2
+    
+  // })
+  let tl = gsap.timeline({
+    // yes, we can add it to an entire timeline!
+    scrollTrigger: {
+      trigger: ".cont",
+      pin: true,   // pin the trigger element while active
+      start: "top top", // when the top of the trigger hits the top of the viewport
+      end: "+=500", // end after scrolling 500px beyond the start
+      scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+      snap: {
+        snapTo: "labels", // snap to the closest label in the timeline
+        duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+        delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+        ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
+      }
+    }
+  });
+
+// add animations and labels to the timeline
+tl.addLabel("start")
+  .from(".intro-text", {scale: 0.3, rotation:45, autoAlpha: 0})
+  .addLabel("color")
+  .from(".intro-text", {backgroundColor: "#28a92b"})
+  .addLabel("spin")
+  .to(".intro-text", {rotation: 360})
+  .addLabel("end");
