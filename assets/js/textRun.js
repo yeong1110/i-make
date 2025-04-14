eaf859bd19fdcccb50d112685876ca872350b777
yeong1110 <a0257942@gmail.com>
2025-04-14 21:09:48 +0900

    style: main 교체

--- assets/js/textRun.js
+++ assets/js/textRun.js
@@ -1,7 +1,7 @@
 const scrollElement = document.querySelectorAll(".isScroll");
 window.addEventListener("scroll", scrollActive);
 function scrollActive() {
-  console.log(window.pageYOffset);
+  // console.log(window.pageYOffset);
   scrollElement.forEach((el, index) => {
     let elTop = el.getBoundingClientRect().top;
     let elBottom = el.getBoundingClientRect().bottom;
@@ -9,6 +9,7 @@ function scrollActive() {
     if (elTop.toFixed() <= 500 && elBottom.toFixed() >= -1) {
       let persentage = ((elTop - 500) / elHeight) * 100;
       console.log(el.classList);
+      el.classList.add("active");
       if (el.classList.contains("scrollRun1")) {
         el.style.transform = `translateX(-${Math.abs(persentage).toFixed()}%)`;
       }
@@ -17,6 +18,7 @@ function scrollActive() {
       }
     } else {
       el.style.transform = `none`;
+			el.classList.remove("active");
     }
   });
 }
---
+++ assets/js/wobbleText.js
@@ -0,0 +1,22 @@
+const wobbleEl = document.querySelectorAll(".wobble-wrap");
+
+wobbleEl.forEach((el) => {
+	document.addEventListener("mousemove", (e) => {
+		// console.log(e);
+		const filter = el.querySelector("svg feDisplacementMap");
+    const rect = el.querySelector(".wobble-text").getBoundingClientRect();
+    const inX = e.clientX >= rect.left && e.clientX <= rect.right;
+    const inY = e.clientY >= rect.top && e.clientY <= rect.bottom;
+
+    if (inX && inY) {
+      const dx = (e.clientX - rect.left) / rect.width;
+      const dy = (e.clientY - rect.top) / rect.height;
+
+      // 마우스 위치 기반으로 scale 조절
+      const scale = 30 * (1 - Math.hypot(dx - 0.5, dy - 0.5));
+      filter.setAttribute("scale", scale);
+    } else {
+      filter.setAttribute("scale", 0);
+    }
+  });
+});
