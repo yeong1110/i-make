c2a1d3acdb60a89d84ee268bafb2dcd0146a543b
yeong1110 <a0257942@gmail.com>
2025-04-14 17:20:55 +0900

    style: text run script 추가, gsap 삭제

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,5 @@
-gsap.registerPlugin(ScrollTrigger);
 AOS.init();
 
-gsap.set(".intro-cont span", { opacity: 0 });
-gsap.set(".we-animate-cont h3", { opacity: 0, y: -50 });
-gsap.set(".we-animate-2 h4", { opacity: 0 });
-gsap.set(".intro-after-wrap", { opacity: 0 });
-// gsap.set(".we-elm", { y: 100, opacity: 0 });
-
-let delay = 0;
-let dur = 1.5;
-let tp = "150%";
 const address = document.getElementById("myAddress");
 const myAlert = document.querySelector("#alert");
 const btt = document.querySelector("#go-top");
@@ -45,7 +35,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // 갤러리
   const img2 = document.querySelector(".imagine-cont");
-  const img1 = document.querySelectorAll(".imagine-cont li img");
   lightbox = document.querySelector(".lightbox-overlay");
   lightboxContent = document.querySelector(".lightbox-content");
 
@@ -94,76 +83,25 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".intro-wrap",
-      pin: true,
-      start: "top top",
-      end: "+=800",
-      scrub: 1,
-      snap: {
-        snapTo: "labels",
-        duration: 1,
-        delay: 0.2,
-        ease: "power1.inOut",
-      },
-
-      // markers: true
-    },
-  });
-
-  // add animations and labels to the timeline
-  tl.addLabel("start")
-    .from(".intro-text-1", { scale: 0.3, autoAlpha: 1 })
-    .addLabel("text-2")
-    .to(".intro-text-2", { opacity: 1 })
-    .addLabel("text-2-2")
-    .to(".intro-text-2", { scale: 0.3, opacity: 0 })
-    .addLabel("text-3")
-    .to(".intro-text-3", { opacity: 1 })
-    .addLabel("text-3-2")
-    .to(".intro-text-3", { opacity: 0 })
-    .addLabel("text-4")
-    .to(".intro-text-4", { opacity: 1 })
-    .addLabel(".s")
-    .to(".intro-after-wrap", { autoAlpha: 1 });
-
-  let we = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".we-animate-1",
-      // pin: true,
-      start: "-=1500",
-      end: "+=500",
-      snap: {
-        snapTo: "labels",
-        duration: 1.5,
-        delay: 4,
-        ease: "power1.inOut",
-      },
-      // markers: true
-    },
-    translate: "none",
-  });
-
-  we.addLabel("start")
-    .from(".i-1", { autoAlpha: 0 })
-    .addLabel("start1")
-    .to(".i-1", { x: 0, y: 0, scale: 1.2 })
-    .addLabel("start2")
-    .from(".i-2", { autoAlpha: 0 })
-    .to(".i-2", { rotation: -15.924, x: 0, y: 0, scale: 1.2 })
-    .addLabel("start3")
-    .from(".i-3", { autoAlpha: 0 })
-    .to(".i-3", { rotation: -14.849, x: 0, y: 0, scale: 1.2 })
-    .addLabel("start4")
-    .from(".i-4", { autoAlpha: 0 })
-    .to(".i-4", { rotation: 23.129, x: 0, y: 0, scale: 1.2 })
-    .addLabel("end")
-    .to(".i-1", { rotation: 18 })
-    .from(".i-5", { autoAlpha: 0, x: 0, y: 0 })
-    .to(".i-5", { x: 0, y: 0 })
-    .to(".we-animate-1", { opacity: 0, display: "none" })
-    .to(".we-animate-2 h4", { opacity: 1 });
+  // we.addLabel("start")
+  //   .from(".i-1", { autoAlpha: 0 })
+  //   .addLabel("start1")
+  //   .to(".i-1", { x: 0, y: 0, scale: 1.2 })
+  //   .addLabel("start2")
+  //   .from(".i-2", { autoAlpha: 0 })
+  //   .to(".i-2", { rotation: -15.924, x: 0, y: 0, scale: 1.2 })
+  //   .addLabel("start3")
+  //   .from(".i-3", { autoAlpha: 0 })
+  //   .to(".i-3", { rotation: -14.849, x: 0, y: 0, scale: 1.2 })
+  //   .addLabel("start4")
+  //   .from(".i-4", { autoAlpha: 0 })
+  //   .to(".i-4", { rotation: 23.129, x: 0, y: 0, scale: 1.2 })
+  //   .addLabel("end")
+  //   .to(".i-1", { rotation: 18 })
+  //   .from(".i-5", { autoAlpha: 0, x: 0, y: 0 })
+  //   .to(".i-5", { x: 0, y: 0 })
+  //   .to(".we-animate-1", { opacity: 0, display: "none" })
+  //   .to(".we-animate-2 h4", { opacity: 1 });
 });
 
 const setSwiper = (id, object) => {
---
+++ assets/js/textRun.js
@@ -0,0 +1,22 @@
+const scrollElement = document.querySelectorAll(".isScroll");
+window.addEventListener("scroll", scrollActive);
+function scrollActive() {
+  console.log(window.pageYOffset);
+  scrollElement.forEach((el, index) => {
+    let elTop = el.getBoundingClientRect().top;
+    let elBottom = el.getBoundingClientRect().bottom;
+    let elHeight = elBottom - elTop;
+    if (elTop.toFixed() <= 500 && elBottom.toFixed() >= -1) {
+      let persentage = ((elTop - 500) / elHeight) * 100;
+      console.log(el.classList);
+      if (el.classList.contains("scrollRun1")) {
+        el.style.transform = `translateX(-${Math.abs(persentage).toFixed()}%)`;
+      }
+      if (el.classList.contains("scrollRun2")) {
+        el.style.transform = `translateX(${Math.abs(persentage).toFixed()}%)`;
+      }
+    } else {
+      el.style.transform = `none`;
+    }
+  });
+}
