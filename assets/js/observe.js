c9ca6f3738f2afe380831cd5fd669bafbf641b84
yeong1110 <a0257942@gmail.com>
2025-04-16 16:37:02 +0900

    style: observe scroll 추가

---
+++ assets/js/observe.js
@@ -0,0 +1,14 @@
+const srcollEl = document.querySelectorAll(".scroll");
+const observer = new IntersectionObserver((e) => {
+  e.forEach((el) => {
+    el.isIntersecting ? el.target.classList.add("active") : el.target.classList.remove("active");
+    // if (el.isIntersecting) {
+    //   el.target.classList.add("active");
+    // } else {
+    //   el.target.classList.remove("active");
+    // }
+  });
+});
+srcollEl.forEach((el) => {
+  observer.observe(el);
+});
