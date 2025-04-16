3a396615633e4bf54342342f8047f100b7f7df5a
yeong1110 <a0257942@gmail.com>
2025-04-16 17:22:59 +0900

    style: interative 수정

--- assets/js/textRun.js
+++ assets/js/textRun.js
@@ -8,17 +8,21 @@ function scrollActive() {
     let elHeight = elBottom - elTop;
     if (elTop.toFixed() <= 500 && elBottom.toFixed() >= -1) {
       let persentage = ((elTop - 500) / elHeight) * 100;
+      const p100 = Math.abs(persentage).toFixed();
       // console.log(el.classList);
       el.classList.add("active");
       if (el.classList.contains("scrollRun1")) {
-        el.style.transform = `translateX(-${Math.abs(persentage).toFixed()}%)`;
+        el.style.transform = `translateX(-${p100}%)`;
       }
       if (el.classList.contains("scrollRun2")) {
-        el.style.transform = `translateX(${Math.abs(persentage).toFixed()}%)`;
+        el.style.transform = `translateX(${p100}%)`;
+      }
+      if (el.classList.contains("interactive-list")) {
+        el.style.transform = `translate3d(-${p100}%,${p100 / 100}px,10px)`;
       }
     } else {
       el.style.transform = `none`;
-			el.classList.remove("active");
+      el.classList.remove("active");
     }
   });
 }
